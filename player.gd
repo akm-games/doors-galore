e2de73580ab8c2d64f74ef49817bc9e47437e5e4
JOYCE <alexjoyce1111@gmail.com>
2023-01-15 14:27:33 -0500

    replace player sprite

--- player.gd
+++ player.gd
@@ -5,16 +5,16 @@ export (int) var speed = 300
 var velocity = Vector2()
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed("right"):
-        velocity.x += 1
-    if Input.is_action_pressed("left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("down"):
-        velocity.y += 1
-    if Input.is_action_pressed("up"):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
 
 func _ready():
@@ -23,5 +23,5 @@ func _ready():
 
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	get_input()
+	velocity = move_and_slide(velocity)
