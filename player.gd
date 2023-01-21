e12e62db45e82ae568bf308e05dfa19df92ee82b
Khoa Dang Hoang <koadahoakodah@gmail.com>
2023-01-21 12:51:46 -0500

    Move player right on door click

--- player.gd
+++ player.gd
@@ -2,7 +2,12 @@ extends KinematicBody2D
 
 export (int) var speed = 300
 
-var velocity = Vector2()
+var velocity = Vector2();
+var is_moving_right = false
+
+func move_right():
+  velocity = Vector2();
+  velocity.x = speed
 
 func get_input():
   velocity = Vector2()
@@ -16,6 +21,8 @@ func get_input():
     velocity.y -= 1
   velocity = velocity.normalized() * speed
 
+func _init():
+  is_moving_right = false
 
 func _ready():
   var vec2_viewport = get_viewport_rect().size
@@ -23,5 +30,21 @@ func _ready():
 
 
 func _physics_process(delta):
-  get_input()
+  velocity = Vector2()
+  if is_moving_right:
+    move_right()
   velocity = move_and_collide(velocity * delta)
+
+func _on_Door_1_input_event(viewport, event, shape_idx):
+  if event.is_action_released("select"):
+    is_moving_right = true
+
+
+func _on_Door_2_input_event(viewport, event, shape_idx):
+  if event.is_action_released("select"):
+    is_moving_right = true
+
+
+func _on_Door_3_input_event(viewport, event, shape_idx):
+  if event.is_action_released("select"):
+    is_moving_right = true
